20c84227db21e9bf0cf84de6785d4429a92ad51e
Tomasz Okniński <okninski.t2004@gmail.com>
2024-08-21 12:42:57 +0200

    Project setup

---
+++ src/main/java/com/tomcode/TomcodeApplication.java
@@ -0,0 +1,13 @@
+package com.tomcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomcode/TomcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
